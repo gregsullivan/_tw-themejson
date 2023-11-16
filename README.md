e7704f5ce6324e0b2eadd67625a2640df18f8d2b
Greg Sullivan <greg@gregsullivan.com>
2023-11-16 11:38:01 -0800

    Change approach to loading `theme.json` file Due to changes in how Tailwind executes plugins, the `theme.json` file is now cached when loaded via `require`, preventing updates. These changes allow for the path to be passed as a string and loaded via the filesystem.
    This change is meant to be backwards-compatible with the existing approach.

--- README.md
+++ README.md
@@ -5,10 +5,30 @@ A Tailwind CSS plugin that automatically pulls colors and widths from a WordPres
 ```js
 // tailwind.config.js
 plugins: [
-	require( '@_tw/themejson' )( require( 'path/to/theme.json' ) ),
+	require('@_tw/themejson'),
 ]
 ```
 
-This will extract the base color palette (`settings.color.palette`) as well as values for `contentSize` and `wideSize`.
+By default, the `theme.json` file will be loaded from `./theme/theme.json`, relative to the current working directory. (The default path corresponds with that of [\_tw](https://underscoretw.com).) You can override this location by passing a new path as a string:
+
+```js
+// tailwind.config.js
+plugins: [
+	require('@_tw/themejson')('path/to/theme.json'),
+]
+```
+
+You can also parse `theme.json` yourself and pass in the corresponding object:
+
+```js
+// tailwind.config.js
+plugins: [
+	require('@_tw/themejson')(require('path/to/theme.json')),
+]
+```
+
+You may need to manage caching should you choose to use `require` to parse the JSON file.
+
+After initiation, the plugin will extract the base color palette (`settings.color.palette`) as well as values for `contentSize` and `wideSize`.
 
 If one or more of your color palette’s `slug` values matches a slug in Tailwind’s default color palette, those colors from the default palette—including all shades—will be overridden by the `color` value from your `theme.json` file.
--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-const plugin = require( 'tailwindcss/plugin' );
+const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin.withOptions(
-	function() {
+	function () {
 		// Use an empty plugin function to accept the options object.
-		return function() {};
-	}, function( themejson ) {
+		return function () {};
+	},
+	function (themejson) {
+		// If the theme.json object is not provided, attempt to load it.
+		if (typeof themejson !== 'object' || themejson === null) {
+			const fs = require('fs');
+			const path = require('path');
+			let themejsonPath;
+
+			if (typeof themejson === 'string') {
+				// If `themejson` is a string, use it as the path to the theme.json
+				// file relative to the current working directory.
+				themejsonPath = path.join(process.cwd(), themejson);
+			} else {
+				// Otherwise, use the default path from _tw.
+				themejsonPath = path.join(process.cwd(), 'theme/theme.json');
+			}
+
+			if (fs.existsSync(themejsonPath)) {
+				try {
+					themejson = JSON.parse(
+						fs.readFileSync(themejsonPath, 'utf8')
+					);
+				} catch (e) {
+					// Handle JSON parsing error.
+					console.error('Error parsing JSON:', e);
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+
 		// Create an empty object for the theme.json properties.
 		const themejsonProps = {};
 
 		// Load all of the colors from the theme.json file.
-		if ( Array.isArray( themejson.settings.color.palette ) ) {
+		if (Array.isArray(themejson.settings.color.palette)) {
 			themejsonProps.colors = {};
-			themejson.settings.color.palette.forEach( function( color ) {
-				themejsonProps.colors[ color.slug ] = color.color;
-			} );
+			themejson.settings.color.palette.forEach(function (color) {
+				themejsonProps.colors[color.slug] = color.color;
+			});
 		}
 
 		// Load all of the widths from the theme.json file.
 		themejsonProps.widths = {};
-		[ 'content', 'wide' ].forEach( function( width ) {
-			if ( undefined !== themejson.settings.layout[ width + 'Size' ] ) {
-				themejsonProps.widths[ width ] = themejson.settings.layout[ width + 'Size' ];
+		['content', 'wide'].forEach(function (width) {
+			if (undefined !== themejson.settings.layout[width + 'Size']) {
+				themejsonProps.widths[width] =
+					themejson.settings.layout[width + 'Size'];
 			}
-		} );
+		});
 
 		// Update the configuration.
 		return {
@@ -37,5 +69,5 @@ module.exports = plugin.withOptions(
 				},
 			},
 		};
-	},
+	}
 );
